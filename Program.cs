4494ccab429aa893ad674a6e137b1c6eb053bdd5
Michael Yearsley <59950440+michaelyearsley@users.noreply.github.com>
2020-02-11 23:21:36 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Survey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int CurrentYear = DateTime.Today.Year;
+            // Ask user for name
+            var name = Sup.Ask("What is your name? ");
+            // Ask user for age            
+            var age = int.Parse(Sup.Ask("What is your age? "));
+            // Ask user for month
+            var month = Sup.Ask("What is your birth month? ");
+            //Ask birth day
+            var day = int.Parse(Sup.Ask("What day where you born? "));
+            var zodiac = Sup.Zodiac(month, day);
+            Console.WriteLine(zodiac);
+            // print the information
+            Console.WriteLine("Your name is: {0} \n you were born on {1} {2}, {3} your Zodiac is {4}", name, month, day,(CurrentYear-age), zodiac);
+          
+            
+        }
+
+        
+    }
+}
---
+++ Sup.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Survey
+{
+    class Sup
+    {
+        static public string Ask(string question)
+        {
+            Console.Write(question);
+            question =Console.ReadLine();
+
+            if (question == "")
+            {
+                question = TryAgain();
+
+            }
+            return question;
+        }
+        static string TryAgain()
+        {
+            Console.WriteLine("You didn't type anything, please try again:");
+            return Console.ReadLine();
+        }
+        static public string Zodiac(string month, int day)
+        {
+            string zodiac = "";
+            month.ToLower();
+            switch (month)
+            {
+                case "january":
+                    if (0 <= day && day <= 19)
+                    {
+                        zodiac = "Capricorn";
+                    }
+                    else if (20 <= day && day <= 31)
+                    {
+                        zodiac = "Aquarius";
+                    }
+                    break;
+                case "february":
+                    if (0 <= day && day <= 19)
+                    {
+                        zodiac = "Aquarius";
+                    }
+                    else if (20 <= day && day <= 28)
+                    {
+                        zodiac = "Pisces";
+                    }
+                    break;
+                case "march":
+                    if (0 <= day && day <= 20)
+                    {
+                        zodiac = "Pisces";
+                    }
+                    else if (21 <= day && day <= 31)
+                    {
+                        zodiac = "Aries";
+                    }
+                    break;
+                case "april":
+                    if (0 <= day && day <= 19)
+                    {
+                        zodiac = "Aries";
+                    }
+                    else if (20 <= day && day <= 30)
+                    {
+                        zodiac = "Taurus";
+                    }
+                    break;
+                case "may":
+                    if (0 <= day && day <= 20)
+                    {
+                        zodiac = "Taurus";
+                    }
+                    else if (21 <= day && day <= 31)
+                    {
+                        zodiac = "Gemini";
+                    }
+                    break;
+                case "june":
+                    if (0 <= day && day <= 21)
+                    {
+                        zodiac = "Gemini";
+                    }
+                    else if (21 <= day && day <= 30)
+                    {
+                        zodiac = "Cancer";
+                    }
+                    break;
+                case "july":
+                    if (0 <= day && day <= 22)
+                    {
+                        zodiac = "Cancer";
+                    }
+                    else if (21 <= day && day <= 30)
+                    {
+                        zodiac = "Leo";
+                    }
+                    break;
+
+            }
+            
+            return zodiac;
+        }
+    }
+        
+}
